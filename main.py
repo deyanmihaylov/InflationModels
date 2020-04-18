85d87a4e7b45e6af2011704e67f1a09b4964e7c5
deyan.mihaylov <d.p.mihaylov@gmail.com>
2020-04-19 00:12:01 +0200

    New functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-# from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp
 
 import pygsl.rng
 
@@ -49,10 +49,109 @@ def pick_init_vals ():
     
     return init_vals, init_Nefolds
 
-# Fills vector * yinit with randomly chosen initial values of the flow
-# parameters, as well as a randomly chosen amount of inflation, Nefolds.
+def derivs(t, y):
+    dydN = np.zeros(NEQS , dtype=float , order='C')
+    
+    if y[2] >= 1.0:
+        dydN = np.zeros(NEQS , dtype=float , order='C')
+    else:
+        if y[2] > VERYSMALLNUM:
+            dydN[0] = - np.sqrt ( y[2] / (4 * np.pi ) )
+        else:
+            dydN[0] = 0.0
+        
+        dydN[1] = y[1] * y[2]
+        dydN[2] = y[2] * ( y[3] + 2.0 * y[2] )
+        dydN[3] = 2.0 * y[4] - 5.0 * y[2] * y[3] - 12.0 * y[2] * y[2]
+        
+        for i in range(4, NEQS-1):
+            dydN[i] = ( 0.5 * (i-3) * y[3] + (i-4) * y[2] ) * y[i] + y[i+1]
+            
+        dydN[NEQS-1] = ( 0.5 * (NEQS-4) * y[3] + (NEQS-5) * y[2] ) * y[NEQS-1]
+    
+    return dydN
 
-# POSSIBLE ISSUE WITH EXACT DECIMAL NUMBERS
+def int_de(y, N, Nend, derivs):
+    h = 1e-6
+    ydoub = y.copy()
+        
+    Nsol = solve_ivp(derivs, (N , Nend), ydoub, method='RK45', t_eval=None, dense_output=False, events=None, vectorized=False , first_step=h)
+    
+    sol_status = Nsol['status']
+    sol_length = len ( Nsol['t'] )
+    sol_x = Nsol['t']
+    sol_Y = Nsol['y']
+    
+    if sol_length > kmax:
+        sol_length = kmax
+        sol_x = sol_x [0:kmax]
+        sol_Y = sol_Y [: , 0:kmax]
+        
+    return sol_status , sol_length , sol_x , sol_Y
+
+VERYSMALLNUM = 1e-18
+LOTSOFEFOLDS = 1000.0
+
+def calcpath(Nefolds, y, path, N, count):
+    retval = "internal_error"
+    
+    # Check to make sure we are calculating to sufficient order.
+    if NEQS < 6:
+        raise Exception("calcpath(): NEQS must be at least 6\n")
+        sys.exit()
+    
+    # Allocate buffers for integration.
+    # dydN = derivatives of flow functions wrt N
+    # yp = intermediate values for y
+    # xp = intermediate values for N
+    yp = np.zeros((NEQS, kmax), dtype=float, order='C')
+    xp = np.zeros(kmax, dtype=float, order='C')
+    
+    # First find the end of inflation, when epsilon crosses through unity.
+    Nstart = LOTSOFEFOLDS
+    Nend = 0.0
+    
+    z, kount, xp, yp = int_de(y, Nstart, Nend, derivs)
+
+    print(z, kount, xp, yp)
+    exit()
+    
+    y = yp [:, -1]
+    
+    if z < 0:
+        retval = "internal_error"
+        z = 0
+#         goto end;
+    else:
+#         Find when epsilon passes through unity.
+        i = check_convergence ( yp , kount )
+        
+        if not i:
+#             We never found an end to inflation, so we must be at a late-time attractor.
+            if y[2] > SMALLNUM or y[3] < 0.0:
+#                 The system did not evolve to a known asymptote.
+                retval = "noconverge"
+            else:
+                retval = "asymptote"
+        else:
+#             We found an end to inflation: integrate backwards Nefolds e-folds from that point.
+            Nstart = xp[i-2] - xp[i-1]
+            Nend = Nefolds
+            
+            y = yp [ : , i-2 ].copy()
+            
+            z , kount , xp , yp = int_de ( y , Nstart , Nend , derivs )
+            
+            if z < 0:
+                retval = "internal_error"
+                z = 0
+            else:
+                if check_convergence ( yp , kount ):
+                    retval = "insuff"
+                else:
+                    retval = "nontrivial"
+                    
+    print (retval)
 
 def main():
     calc = Calc()
@@ -76,6 +175,9 @@ def main():
     # allocate buffers
     y = np.zeros(NEQS, dtype=float, order='C')
     yinit = np.zeros(NEQS, dtype=float, order='C')
+    N = np.array([])
+
+    path = np.array([[]])
 
     # iters = total number of iterations
     iters = 0
@@ -115,9 +217,10 @@ def main():
                 print(".")
 
         yinit, calc.Nefolds = pick_init_vals()
-        
-        for y in yinit:
-            print(y)
+
+        y = yinit.copy()
+
+        calc.ret = calcpath(calc.Nefolds, y, path, N, calc.npoints)
 
         break
 
@@ -155,103 +258,7 @@ if __name__ == "__main__":
 
 """
 
-def derivs (t, y):
-    dydN = numpy.zeros(NEQS , dtype=float , order='C')
-    
-    if y[2] >= 1.0:
-        dydN = numpy.zeros(NEQS , dtype=float , order='C')
-    else:
-        if y[2] > VERYSMALLNUM:
-            dydN[0] = - numpy.sqrt ( y[2] / (4 * numpy.pi ) )
-        else:
-            dydN[0] = 0.0
-        
-        dydN[1] = y[1] * y[2]
-        dydN[2] = y[2] * ( y[3] + 2.0 * y[2] )
-        dydN[3] = 2.0 * y[4] - 5.0 * y[2] * y[3] - 12.0 * y[2] * y[2]
-        
-        for i in range (4 , NEQS-1):
-            dydN[i] = ( 0.5 * (i-3) * y[3] + (i-4) * y[2] ) * y[i] + y[i+1]
-            
-        dydN[NEQS-1] = ( 0.5 * (NEQS-4) * y[3] + (NEQS-5) * y[2] ) * y[NEQS-1]
-    
-    return dydN
 
-def int_de (y , N , Nend , derivs):
-    h = 1e-6
-    ydoub = y.copy()
-        
-    Nsol = solve_ivp(derivs, (N , Nend), ydoub, method='RK45', t_eval=None, dense_output=False, events=None, vectorized=False , first_step=h)
-    
-    sol_status = Nsol['status']
-    sol_length = len ( Nsol['t'] )
-    sol_x = Nsol['t']
-    sol_Y = Nsol['y']
-    
-    if sol_length > kmax:
-        sol_length = kmax
-        sol_x = sol_x [0:kmax]
-        sol_Y = sol_Y [: , 0:kmax]
-        
-    return sol_status , sol_length , sol_x , sol_Y
-
-def calcpath (Nefolds , y , path , count):
-# 	double Nstart,Nend;
-# 	gsl_vector * xp;
-# 	gsl_matrix * yp;
-# 	enum calcpath_retcode retval=internal_error;
-# 	int i,j,k,z,kount;
-# 	double Hnorm;
-
-    retval = "internal_error"
-    
-    if NEQS < 6:
-        raise Exception("calcpath(): NEQS must be at least 6\n")
-    
-    yp = numpy.zeros((NEQS , kmax) , dtype=float , order='C')
-    xp = numpy.zeros(kmax , dtype=float , order='C')
-    
-    Nstart = LOTSOFEFOLDS
-    Nend = 0.0
-    
-    z , kount , xp , yp = int_de ( y , Nstart , Nend , derivs )
-    
-    y = yp [ : , -1 ]
-    
-    if z < 0:
-        retval = "internal_error"
-        z = 0
-#         goto end;
-    else:
-#         Find when epsilon passes through unity.
-        i = check_convergence ( yp , kount )
-        
-        if not i:
-#             We never found an end to inflation, so we must be at a late-time attractor.
-            if y[2] > SMALLNUM or y[3] < 0.0:
-#                 The system did not evolve to a known asymptote.
-                retval = "noconverge"
-            else:
-                retval = "asymptote"
-        else:
-#             We found an end to inflation: integrate backwards Nefolds e-folds from that point.
-            Nstart = xp[i-2] - xp[i-1]
-            Nend = Nefolds
-            
-            y = yp [ : , i-2 ].copy()
-            
-            z , kount , xp , yp = int_de ( y , Nstart , Nend , derivs )
-            
-            if z < 0:
-                retval = "internal_error"
-                z = 0
-            else:
-                if check_convergence ( yp , kount ):
-                    retval = "insuff"
-                else:
-                    retval = "nontrivial"
-                    
-    print (retval)
 
 def check_convergence (yy , kount):
     for i in range (kount):
@@ -273,7 +280,7 @@ NUMEFOLDSMAX = 60.0
 NUMEFOLDSMIN = 40.0
 
 SMALLNUM = 0.0001
-VERYSMALLNUM = 1e-18
+
 LOTSOFEFOLDS = 1000.0
 
 calc = Calc()
